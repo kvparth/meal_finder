04850dcef45a8b3e715eb13cf1b34659d4ff952e
kvparth <parthkvk10@gmail.com>
2022-03-03 04:23:52 +0530

    almostdone3.0

--- src/App.js
+++ src/App.js
@@ -22,37 +22,47 @@ export default function App() {
   const handleSelect = (item) => {
     setSelect(item);
   };
+  const randomMeal=()=>{
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`).then((response)=>response.json()).then((data)=>{
+      handleSelect(data.meals[0]);
+      addIngredients(data.meals[0])   
+  })
+  }
   const addIngredients=(item)=>{
-    console.log(ingredients);
-    console.log(item);
+    // console.log(ingredients);
+    // console.log(item);
     let addedIngredients = []
   if(item!==null){
     Object.keys(item).forEach((object,i)=>{
       if (item[`strIngredient${i}`]){
-         //setIngredients([...ingredients,`${item[`strIngredient${i}`]} - ${item[`strMeasure${i}`]}`])
          addedIngredients.push(`${item[`strIngredient${i}`]} - ${item[`strMeasure${i}`]}`)
       }
     })
-    console.log('addedIngredients', addedIngredients)
+    // console.log('addedIngredients', addedIngredients)
     setIngredients(addedIngredients)
   }
 }
-  console.log(ingredients);
-  console.log(meals);
+  // console.log(ingredients);
+  // console.log(meals);
   return (
     <div className="App">
-      <h1>Meal Finder</h1>
-      <input type="text" value={input} onChange={(e) => handlechange(e)} />
+      <h1 style={{color:"221260"}}>Meal Finder</h1>
+      <div>
+        <form  id="submit">
+      <input type="text" value={input} onChange={(e) => handlechange(e)} onSubmit={searchHandle}/>
+      </form>
       <button onClick={searchHandle}>Search</button>
+      <button onClick={randomMeal}>Shuffle</button>
+      </div>
       {meals=== null ? (
-        <div>No Data Found</div>
+        <div><h3>No Result Found TRY AGAIN!</h3></div>
       ) : (
         <List meals={meals} onSelect={handleSelect}
         addIngredients={addIngredients} />
       )}
       {select !== null   && <div>
         <h1>{select?.strMeal}</h1>
-        <img src={select?.strMealThumb} alt="img" />
+        <img id="single-meal" src={select?.strMealThumb} alt="img" />
         <div>
           <p>Category:{select?.strCategory}</p>
           <p>Region:{select?.strArea}</p>
@@ -60,7 +70,7 @@ export default function App() {
         <p>{select?.strInstructions}</p>
         <h2>Ingredients</h2>
         <ul>
-          {ingredients.map((ing)=><li>{ing}</li>)}
+          {ingredients.map((ing)=><li key={Math.random()*20}>{ing}</li>)}
         </ul>
         </div>}
     </div>
--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -3,6 +3,7 @@ const List = (props) => {
   return (
     <div className="imgContainer">
       {props.meals.map((item) => (
+        <div className="meal" >
         <img
           className="img"
           src={item.strMealThumb}
@@ -12,8 +13,12 @@ const List = (props) => {
             props.addIngredients(item);
           
           }}
-          alt="ima"
+          alt={item.strMeal}
         />
+        <div className="meal-info">
+          <h3 className="h4">{item.strMeal}</h3>
+        </div>
+        </div>
       ))}
     </div>
   );
